3ad13989532fe86621919ef8b14b437f8750e883
stephenbowyer <github@ste.widearea.org>
2025-09-30 00:40:20 +0100

    Corrected Media Type of API listing

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/APIController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/APIController.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,9 @@ public class APIController {
         try {
             String content = new String(Files.readAllBytes(
                 Paths.get("src/main/endpoints.json")));
-            return ok(content);
+            return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(content);
         } catch (IOException e) {
             return ResponseEntity.status(500).body("Failed to read endpoints.json");
         }
